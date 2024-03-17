8bf07c672d4396db068443a107d697e331fae1cb
Frathol <michaelmosses35746@gmail.com>
2024-03-17 19:09:09 +0700

    kontol

--- Statistics/main.py
+++ Statistics/main.py
@@ -2,47 +2,30 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-"""
-# plt.style.use('ggplot')
-#pd.set_option('max_columns',200)"""
 
-
-# Tentukan path lengkap ke file 'titanic.csv'
-file_path = 'C:/path/to/your/directory/titanic.csv'
-
-# Baca dataset Titanic dari file 'titanic.csv'
-titanic_data = pd.read_csv(file_path)
-
-titanic_data = pd.read_csv('C:/path/to/your/titanic.csv')
-
-# Membaca dataset Titanic
-titanic_data = pd.read_csv('titanic.csv')
+# Membaca dataset Iris
+iris_data = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
 
 # Menampilkan beberapa baris pertama dari dataset
-print(titanic_data.head())
+print(iris_data.head())
 
 # Melihat ringkasan statistik deskriptif dari dataset
-print(titanic_data.describe())
+print(iris_data.describe())
 
 # Melihat distribusi variabel numerik dengan histogram
-titanic_data.hist(figsize=(10,8))
+iris_data.hist(figsize=(10,8))
+plt.show()
+
+# Membuat Pairplot untuk melihat hubungan antar variabel
+sns.pairplot(iris_data, hue='species')
 plt.show()
 
 # Membuat Boxplot untuk melihat distribusi dan outlier
 plt.figure(figsize=(10,8))
-sns.boxplot(data=titanic_data[['Age', 'Fare']])
+sns.boxplot(data=iris_data.drop(columns='species'))
 plt.show()
 
 # Melihat korelasi antar variabel numerik
 plt.figure(figsize=(10,8))
-sns.heatmap(titanic_data.corr(), annot=True, cmap='coolwarm')
+sns.heatmap(iris_data.corr(), annot=True, cmap='coolwarm')
 plt.show()
-
-# Membuat diagram batang untuk variabel kategorikal
-plt.figure(figsize=(10,8))
-sns.countplot(x='Survived', data=titanic_data, hue='Sex')
-plt.show()
-"""
-
-df = pd.read_csv('https://drive.usercontent.google.com/u/0/uc?id=19cTc2gQO_V0UBgpK1tq9QcnjuLxVqznY&export=download')
-df.head()"""
